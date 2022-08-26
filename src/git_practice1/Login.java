178d995df44c42f259f2bf42d757ad00cb3bbbd0
Alec1 <alec0505@gmail.com>
2022-08-26 09:42:15 -0400

    created package and added some more code

--- src/git_practice1/Login.java
+++ src/git_practice1/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("vacation");
+        System.out.println("iphone");
     }
 }
